d1955262692e6cbe8a611f02e52646657e80a2b7
Kauã Victor <164793271+kauavictorss@users.noreply.github.com>
2025-11-21 11:29:06 -0300

    Adicionando configurações do maven

---
+++ src/main/java/sispradia/api/SispradiaApplication.java
@@ -0,0 +1,11 @@
+package sispradia.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispradiaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SispradiaApplication.class, args);
+    }
+}
